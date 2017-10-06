86551d2f4f8a7861c5203e286df9c85996e8ee36
rafaelpllopes <rafael.pllopes@gmail.com>
2017-10-06 10:01:28 -0300

    Alterar versaoView

--- js/controller/SipniController.js
+++ js/controller/SipniController.js
@@ -1,7 +1,7 @@
 class SipniController {
     constructor() {
         this._versao = new Versao();
-        this._versaoView = new VersaoView('#vs-sistema');
+        this._versaoView = new VersaoView('#home');
         this._versaoView.update(this._versao);
     }
 }
--- js/view/VersaoView.js
+++ js/view/VersaoView.js
@@ -1,7 +1,14 @@
 class VersaoView extends View {
     template(model) {
-        return `<li><a href="#">Aplicação: ${model.app}</a></li>
-        <li><a href="#">Banco de dados: ${model.db}</a></li>
-        <li><a href="#">Instâncias: ${model.inst}</a></li>`;
+        return `<div class="container sipni-bannerWrapper">
+        <div class="sipni-banner">
+            <h2>Ultimas Atualizações</h2>
+            <ul id='vs-sistema'>
+            <li><a href="#">Aplicação: ${model.app}</a></li>
+            <li><a href="#">Banco de dados: ${model.db}</a></li>
+            <li><a href="#">Instâncias: ${model.inst}</a></li>
+            </ul>
+        </div>
+    </div>`;
     }
 }
