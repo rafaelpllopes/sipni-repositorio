77c10dc5cbeed90db9b0ecfbc9d1c649a12b348b
rafaelpllopes <rafael.pllopes@gmail.com>
2017-10-09 15:50:09 -0300

    Criar em na sessao downloads a sessao aplicação, banco de dados, instancias, bairros, uteis. Modificar a classe model/Versão.js adicionar links.

--- js/model/Versao.js
+++ js/model/Versao.js
@@ -3,6 +3,9 @@ class Versao {
         this._app = '5.2.0';
         this._db = '5.5.1';
         this._inst = '7.1.0';
+        this._urlApp = 'http://189.44.213.211:8083/sipni/atualizacao/aplicacao//SIPNIInstWin32(5.2.0).zip';
+        this._urlDb = 'http://189.44.213.211:8083/sipni/atualizacao/tabelas//tabelas_5.5.1.update';
+        this._urlInst = 'http://189.44.213.211:8083/sipni/atualizacao/instancia//instancias_7.1.0.update';
         Object.freeze(this);
     }
 
@@ -16,5 +19,16 @@ class Versao {
 
     get inst() {
         return this._inst;
+    }
+    get urlApp() {
+        return this._urlApp;
+    }
+
+    get urlDb() {
+        return this._urlDb;
+    }
+
+    get urlInst() {
+        return this._urlInst;
     }
 }
--- js/view/VersaoView.js
+++ js/view/VersaoView.js
@@ -4,9 +4,9 @@ class VersaoView extends View {
         <div class="sipni-banner">
             <h2>Ultimas Atualizações</h2>
             <ul id='vs-sistema'>
-            <li><a href="#">Aplicação: ${model.app}</a></li>
-            <li><a href="#">Banco de dados: ${model.db}</a></li>
-            <li><a href="#">Instâncias: ${model.inst}</a></li>
+            <li><a href="${model.urlApp}">Aplicação: ${model.app}</a></li>
+            <li><a href="${model.urlDb}">Banco de dados: ${model.db}</a></li>
+            <li><a href="${model.urlInst}">Instâncias: ${model.inst}</a></li>
             </ul>
         </div>
     </div>`;
