db697ad8640b0637483456dbcee92d90806d8b49
rafaelpllopes <rafael.pllopes@gmail.com>
2017-10-11 09:28:56 -0300

    Atualizar o monta url no backend.

--- server/MontaLista.php
+++ server/MontaLista.php
@@ -7,7 +7,7 @@
 			foreach ($lista->getLista() as $nome) {
 				preg_match('/([_(])(\d{1,}.\d{1,}.\d{1,})/', $nome, $matches, PREG_OFFSET_CAPTURE);
 				array_push($filenames, 
-					array('url' => "http://localhost/sipni-repositorio/server/{$local}/{$nome}", 
+					array('url' => "http://189.44.213.211:8083/sipni/server/{$local}/{$nome}", 
 						'versao' => $matches[2][0]
 					)
 				);
