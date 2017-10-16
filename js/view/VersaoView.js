e251fc3c3e7ce29429dad3b189b1d85acba159c1
rafaelpllopes <rafael.pllopes@gmail.com>
2017-10-16 08:47:37 -0200

    Modificar layout footer, modificar titulo no banner, e adicionar titulo no panel.

--- js/view/VersaoView.js
+++ js/view/VersaoView.js
@@ -2,11 +2,12 @@ class VersaoView extends View {
     template(model) {
         return `<div class="container sipni-bannerWrapper">
         <div class="sipni-banner">
-            <h2>Ultimas Atualizações</h2>
+            <h2>Sipni repositório</h2>
+            <h3>Ultimas atualizações</h3>
             <ul id='vs-sistema'>
-            <li><a href="${model.urlApp}">Aplicação: ${model.app}</a></li>
-            <li><a href="${model.urlDb}">Banco de dados: ${model.db}</a></li>
-            <li><a href="${model.urlInst}">Instâncias: ${model.inst}</a></li>
+                <li><a href="${model.urlApp}">Aplicação: ${model.app}</a></li>
+                <li><a href="${model.urlDb}">Banco de dados: ${model.db}</a></li>
+                <li><a href="${model.urlInst}">Instâncias: ${model.inst}</a></li>
             </ul>
         </div>
     </div>`;
